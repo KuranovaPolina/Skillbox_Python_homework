8a4b7cfa35203cf843e307f01ed7694aa8c0661a
Полина <kuranovapolina@gmail.com>
2021-08-30 19:33:37 +0300

    The code of task 7 has been written

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def digit_count(N, i):
+    count = 0
+    while N > 0:
+        digit = N % 10
+        N //= 10
+        if digit == i:
+            count += 1
+    return count
+
+def same_digit(N):
+    is3 = False
+    for i in range(10):
+        count = digit_count(N, i)
+        if count == 3:
+            is3 = True
+            break
+    return is3
+
+start = int(input("Введите первый год: "))
+end = int(input("Введите второй год: "))
+
+for year in range(start, end + 1):
+    if same_digit(year) == True:
+        print(year)
