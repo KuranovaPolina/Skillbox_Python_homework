63734369446be21512f20bddaa3a6dc519754153
Полина <kuranovapolina@gmail.com>
2021-08-30 19:01:45 +0300

    The code of task 6 has been written

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import math
+
+print("Введите координаты монетки:")
+X = float(input("X: "))
+Y = float(input("Y: "))
+r = float(input("Введите радиус: "))
+
+if math.sqrt(X ** 2 + Y ** 2) <= r:
+    print("Монетка где-то рядом")
+else:
+    print("Монетки в области нет")
