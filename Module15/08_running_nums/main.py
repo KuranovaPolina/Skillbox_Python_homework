55770c7e6c0604c3d87764d16df4725327171d28
Полина <kuranovapolina@gmail.com>
2021-09-11 23:51:17 +0300

    8. Task: run digits - was added

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def shift (digits):
+    last_digit = digits[-1]
+    del digits[-1]
+    digits.insert(0, last_digit)
+
+    return digits
+
+K = int(input("Shift: "))
+
+digits = [1, 4, -3, 0, 10]
+
+print("Start list:", digits)
+
+for _ in range(K):
+    shift(digits)
+
+print("Shifted list:", digits)
