ffdbb7af11d95b7ea04896fae04728c54baba0bf
Полина <kuranovapolina@gmail.com>
2021-09-11 20:17:47 +0300

    5. Task: Creating list of favourite films was added

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,28 @@
+def filmfounder(film, films):
+    find = False
+    for i in films:
+        if i == film:
+            find = True
+    return find
+
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
 
-# TODO здесь писать код
+favourite_films = []
+
+print("List of films: ", films)
+
+print("Adding favourite films. Input 'Add' to finish")
+
+while True:
+    film = input("Film name: ")
+    if film == 'Add':
+        break
+    elif not filmfounder(film, films):
+        print("Error. Film was not found")
+        #break
+    else:
+        favourite_films.append(film)
+
+print("List of favourite films", favourite_films)
