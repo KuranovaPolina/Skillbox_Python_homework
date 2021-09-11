f77d6969e881c8967720cdf49492622f7fb37982
Полина <kuranovapolina@gmail.com>
2021-09-11 22:47:45 +0300

    6. Task: Word analysis was added

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def letter_count(letter, letters_list):
+    count = 0
+
+    for i in letters_list:
+        if i == letter:
+            count += 1
+
+    return count
+
+word = input("Input word: ")
+letters_list = list(word)
+
+count0 = 0
+for letter in letters_list:
+    if letter_count(letter, letters_list) == 1:
+        count0 += 1
+
+print("Number of unic letters:", count0)
