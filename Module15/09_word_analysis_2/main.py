fa78bc2fff6aeedc845240105aae9a2008101c39
Полина <kuranovapolina@gmail.com>
2021-09-12 00:04:18 +0300

    9. Task: Word analysis 2 - was added

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+word = input("Input word: ")
+letters_list = list(word)
+
+lenght = len(letters_list)
+
+steps = lenght // 2
+
+is_polindrom = True
+
+for i in range(steps):
+    if letters_list[i] != letters_list[-(i + 1)]:
+        is_polindrom = False
+        break
+
+if is_polindrom:
+    print("Palindrom")
+else:
+    print("Not palindrom")
