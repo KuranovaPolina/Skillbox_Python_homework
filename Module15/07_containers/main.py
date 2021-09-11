f7cfdeff2bb38fef7e8527ca31efa4270f3e7b3d
Полина <kuranovapolina@gmail.com>
2021-09-11 23:36:44 +0300

    7. Task: containers - was added

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+containers = []
+N = int(input("Number of containers: "))
+
+for _ in range(N):
+    containers.append(int(input("Container's weight: ")))
+
+new_weight = int(input("\nNew container's weight: "))
+
+for i in range(N):
+    if containers[i] < new_weight:
+        print("\nPlace:", i + 1)
+        break
