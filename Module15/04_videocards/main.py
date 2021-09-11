fc6d36de8ac233cc391ef0e2cccc5c1954fbd14d
Полина <kuranovapolina@gmail.com>
2021-09-11 19:50:39 +0300

    4. Video card's task was made

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,22 @@
-# TODO здесь писать код
+video_cards = []
+N = int(input("Nunber of video cards: "))
 
+max = 0
 
+for i in range(1, N + 1):
+    val = int(input(f"{i} video card: "))
+    video_cards.append(val)
+    if val > max:
+        max = val
+
+print("Old list:", video_cards)
+
+tmp_list = []
+
+for card in video_cards:
+    if card != max:
+        tmp_list.append(card)
+
+video_cards = tmp_list
+
+print("New list:", video_cards)
