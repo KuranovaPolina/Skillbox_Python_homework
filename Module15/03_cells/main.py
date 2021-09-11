69f197168d5461ebef0d456a0326952695ccc6c1
Полина <kuranovapolina@gmail.com>
2021-09-11 19:28:33 +0300

    3. Cells task was made

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+cells = []
+N = int(input("N = "))
+
+for i in range(N):
+    cells.append(int(input(f"{i + 1} cell's effectiveness: ")))
+
+print("Bad cells: ", end = '')
+for i in range(N):
+    if cells[i] < i + 1:
+        print(cells[i], end = ' ')
