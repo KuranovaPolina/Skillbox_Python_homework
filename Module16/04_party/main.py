89c6efaf8f8a2893fdd68a8755235e4ae3c51035
Полина <kuranovapolina@gmail.com>
2021-09-18 22:54:58 +0300

    4. party - added

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,20 @@
-guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+guests = ['Petya', 'Vanya', 'Sasha', 'Liza', 'Katya']
 
-# TODO здесь писать код
+while True:
+    lenght = len(guests)
+    print(f"Now there are {lenght} at the party: {guests}")
+    action = input("The guest has come/left: ")
+
+    if action == "go bad":
+        print("The party was finished, everyone are sleeping")
+        break
+    else:
+        name = input("Guest's name: ")
+        if action == 'left':
+            print(f"Bye, {name}\n")
+            guests.remove(name)
+        elif action == 'come' and lenght < 6:
+            print(f"Hi, {name}\n")
+            guests.append(name)
+        else:
+            print(f"Sori, {name}, there are not places\n")
