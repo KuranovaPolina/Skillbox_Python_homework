23e060f716b2eebaf25342cc692b685cc1e077fe
Полина <kuranovapolina@gmail.com>
2021-09-19 20:40:23 +0300

    6. unique elements - added

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+def addlist(list, lenght):
+    for i in range(lenght):
+        list.append(int(input(f"{i+1}: ")))
+
+first_list = []
+second_list = []
+
+addlist(first_list, 3)
+addlist(second_list, 7)
+
+print(first_list)
+print(second_list)
+
+first_list.extend(second_list)
+first_list = list(set(first_list))
+print(first_list)
