491be5febd7d380f7a9e421cedb22abd1948ebe8
Полина <kuranovapolina@gmail.com>
2021-09-20 23:10:46 +0300

    7. rollers - added

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,2 +1,21 @@
-# TODO здесь писать код
-#sfsa
+roller_number = int(input("Roller's namber: "))
+sizes = []
+
+for i in range(roller_number):
+    sizes.append(int(input(f"Size {i + 1}: ")))
+
+people_number = int(input("\nPeople namber: "))
+people = []
+
+for i in range(people_number):
+    people.append(int(input(f"{i + 1} person's size: ")))
+
+count = 0
+for person in people:
+    for size in range(len(sizes)):
+        if sizes[size] >= person:
+            sizes.remove(sizes[size])
+            count += 1
+            break
+
+print("\nAnswer:", count)
