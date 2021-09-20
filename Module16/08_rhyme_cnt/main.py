f65a50c52a9dfdcf39db5dd44486585ce913ca3b
Полина <kuranovapolina@gmail.com>
2021-09-20 23:25:30 +0300

    8. rhyme - added

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,9 +1,22 @@
 people = []
 N = int(input("N = "))
 K = int(input("K = "))
-print(f"Every {K} person\n")
+print(f"Every {K} person")
 
 for i in range(1, N + 1):
     people.append(i)
 
-#while len(people) > 1:
+start = 0
+while len(people) > 1:
+    print(f"\nNow circle: {people}")
+    print(f"Start from {people[start]}")
+
+    delete = (start + K - 1) % len(people)
+    if people[delete] == people[-1]:
+        start = 0
+    else:
+        start = delete
+    print(f"{people[delete]} go out")
+    people.remove(people[delete])
+
+print(f"\n{people[0]} stayed")
