063a963bb5fcf42037530e6f73dfd322955f6e24
Полина <kuranovapolina@gmail.com>
2021-09-26 16:11:18 +0300

    3. random competition - added

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import random
+
+first_list = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second_list = [round(random.uniform(5, 10), 2) for _ in range(20)]
+
+result_list = [max(first_list[i], second_list[i]) for i in range(20)]
+
+print("first team: ", first_list)
+print("second team:", second_list)
+print("result:", result_list)
