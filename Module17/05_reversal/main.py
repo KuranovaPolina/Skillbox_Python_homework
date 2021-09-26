44efda0d3c6cec8404b4423dc3d29fa5dd7760a5
Полина <kuranovapolina@gmail.com>
2021-09-26 16:29:31 +0300

    5. reversal - added

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+string = input("string: ")
+
+first_h = string.index("h")
+second_h = string.rindex("h")
+
+new_string = string[:first_h + 1] + string[second_h - 1: first_h: -1] + string[second_h:]
+
+print("new string:", new_string)
