c5681b7ea4b2628c02926294f04448f9f07a9683
Полина <kuranovapolina@gmail.com>
2021-09-26 18:09:29 +0300

    8. entertainment - added

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+N = int(input("N = "))
+K = int(input("K = "))
+
+sticks = ["I" for _ in range(N)]
+
+for i in range(K):
+    L_i = int(input(f"{i + 1}: start = "))
+    R_i = int(input("end = "))
+
+    sticks[L_i - 1: R_i] = ["." for _ in range(L_i - 1, R_i)]
+
+print("Result: ", end = "")
+for i in sticks:
+    print(i, end = "")
