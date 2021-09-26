fb9230bbf539e4013f6958031f1e7a6a5e5d7782
Полина <kuranovapolina@gmail.com>
2021-09-26 16:51:03 +0300

    6. list compression - added

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+N = int(input("N = "))
+
+print("Input list:")
+first_list = [int(input(f"{i + 1}: ")) for i in range(N)]
+print(first_list)
+
+count0 = first_list.count(0)
+
+second_list = [number for number in first_list if number != 0]
+second_list += [0 for _ in range(count0)]
+
+print(second_list)
+
+third_list = [number for number in second_list if number != 0]
+print(third_list)
