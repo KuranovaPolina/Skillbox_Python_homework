bc83b93ab51fdc7fc3e0860cdc23a9b2602f0db2
Полина <kuranovapolina@gmail.com>
2021-09-26 15:56:08 +0300

    1. vowels - added

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+vowels = ["a", "у", "о", "и", "э","ы", "я", "ю", "е", "ё"]
+
+text = input("Введите текст: ")
+
+text_list = list(text)
+
+text_vowels_list = [letter for letter in text_list if letter in vowels]
+
+print("Список гласных букв:", text_vowels_list)
+print("Длина списка:", len(text_vowels_list))
