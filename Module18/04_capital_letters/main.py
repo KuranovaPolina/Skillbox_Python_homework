58bd55b3e8a187587eea968b49c5cf88ad71e350
Полина <kuranovapolina@gmail.com>
2021-10-01 21:56:10 +0300

    4. Capital letters - modified

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+user_string = input("Input string: ")
+user_string = user_string.lower()
+
+user_string_list = user_string.split(" ")
+
+for i in range(len(user_string_list)):
+    word_list = list(user_string_list[i])
+    word_list[0] = word_list[0].upper()
+    user_string_list[i] = "".join(word_list)
+
+user_string_2 = " ".join(user_string_list)
+
+print("Result:", user_string_2)
