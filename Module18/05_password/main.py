d4287f3521d13e89d08dff403f8dc5d03c0aef80
Полина <kuranovapolina@gmail.com>
2021-10-01 22:18:33 +0300

    5. Password - modified

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def NumberCount(string):
+    count = 0
+    string_list = list(string)
+
+    for letter in string_list:
+        if letter.isdigit():
+            count += 1
+
+    return count
+
+while True:
+    password = input("Password: ")
+
+    if len(password) < 8 or password.islower() or NumberCount(password) < 3:
+        print("Try again")
+
+    else:
+        print("Reliable password")
+        break
