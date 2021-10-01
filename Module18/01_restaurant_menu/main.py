36fe5572fc92f672bd09aabe1e66b2b9a132fa74
Полина <kuranovapolina@gmail.com>
2021-10-01 21:06:56 +0300

    1. Restaurant menu - modified

--- Module18/01_restaurant_menu/main.py
+++ Module18/01_restaurant_menu/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+menu = input("Доступное меню: ")
+
+menu_list = menu.split(";")
+user_menu = ', '.join(menu_list)
+
+print("На данный момент в меню есть: {0}".format(user_menu))
