65ad0cbc190289c1d59cc721b16e82003b1dccc0
Полина <kuranovapolina@gmail.com>
2021-10-01 21:14:32 +0300

    2. Longest word - modified

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+string = input("Input string: ")
+
+string_list = string.split(" ")
+
+maxi = 0
+for word in string_list:
+    if len(word) > maxi:
+        maxi = len(word)
+
+print(maxi)
