1414d770319fb6da6dae57d2b9bbd08b5d629d4f
Полина <kuranovapolina@gmail.com>
2021-10-01 21:32:42 +0300

    3. Files - modified

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+file_name = input("Input file name: ")
+
+if file_name.startswith("@" or "№" or "$" or "%" or "^" or "&" or "*" or "(" or ")"):
+    print("Wrong start")
+elif not file_name.endswith(".txt" or ".docx"):
+    print("Wrong end")
+else:
+    print("Right file's name")
