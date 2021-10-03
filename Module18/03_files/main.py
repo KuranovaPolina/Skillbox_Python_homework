203e5f147ae9c83c2b7dc33a61e27af8fb2809a6
Полина <kuranovapolina@gmail.com>
2021-10-03 19:43:34 +0300

    8. files - modified 2

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,10 +1,9 @@
 file_name = input("Input file name: ")
+wrong_start_letter = tuple("@№$%^&*()")
 
-if file_name.startswith("@" or "№" or "$" or "%" or "^" or "&" or "*" or "(" or ")"):
+if file_name.startswith(wrong_start_letter):
     print("Wrong start")
 elif not file_name.endswith(".txt" or ".docx"):
     print("Wrong end")
 else:
     print("Right file's name")
-
-# TODO: .startswith() если вы хотите передать несколько символов, их нужно передавать в виде кортежа.
--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import string
+
+def back (word):
+
+    word_list = word.split("!" or '"' or "#" or "$" or "%" or "&" or
+                           "'" or "(" or ")" or "*" or "+" or "," or
+                           "-" or "." or "/" or ":" or ";" or "<" or
+                           "=" or ">" or "?" or "@" or "[" or '\ ' or
+                           "]" or "^"or "_" or "`" or "{" or "|" or "}" or "~")
+
+    #for simble in
+
+    return word
+
+message_string = input("Input string: ")
+message_list = message_string.split(" ")
+
+for word in message_list:
+    word = back(word)
