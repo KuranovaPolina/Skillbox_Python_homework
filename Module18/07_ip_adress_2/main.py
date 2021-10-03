f712123b7567362d88a7ddf15b294f70bec76f9a
Полина <kuranovapolina@gmail.com>
2021-10-03 12:50:33 +0300

    7. IP-address 2 - modified

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+IP_string = input("Input IP: ")
+IP_list = IP_string.split(".")
+
+if len(IP_list) != 4:
+    print("IP is 4 numbers separated by dots")
+else:
+    IP_is_rightIP = True
+    for number in IP_list:
+       if not number.isdigit():
+           print("{0} is not a number".format(number))
+           IP_is_rightIP = False
+           break
+       elif int(number) > 255:
+           print("{0} exceeds 255".format(number))
+           IP_is_rightIP = False
+           break
+    if IP_is_rightIP == True:
+        print("IP adress is correct")
