a2cbbe2389880d0ea64a7e0555a2d4df76be63e8
Полина <kuranovapolina@gmail.com>
2021-10-03 12:26:17 +0300

    6. Compression - modified

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+start_string = input("Input string: ")
+count = 0
+new_string = ""
+i = 0
+lens = len(start_string)
+
+while True:
+    letter = start_string[i]
+    count += 1
+
+    if i == lens - 1:
+        new_string += start_string[i] + str(count)
+        break
+    elif start_string[i] != start_string[i + 1]:
+        new_string += start_string[i] + str(count)
+        count = 0
+
+    i += 1
+
+print(new_string)
