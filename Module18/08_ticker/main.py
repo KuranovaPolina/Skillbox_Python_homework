5114ad7e98c0acadba28e14205c0ec585457ff28
Полина <kuranovapolina@gmail.com>
2021-10-03 13:36:23 +0300

    8. ticker - added

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+string1 = input("First string: ")
+string2 = input("Second string: ")
+
+if string1 == string2:
+    print("Strings are equal. Shift: 0")
+elif len(string1) != len(string2):
+    print("Must not")
+else:
+    lens = len(string1)
+    shift = 1
+    must = False
+
+    for i in range(lens):
+        string2 = string2[lens - 1: lens - 2:-1] + string2[:lens-1]
+        if string2 == string1:
+            print("Must. Shift:", shift)
+            must = True
+            break
+        else:
+            shift += 1
+
+    if not must:
+        print("Must not")
