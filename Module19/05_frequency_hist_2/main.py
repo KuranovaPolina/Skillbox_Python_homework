d3d7409316e4cde27d1a02c1d81a44bbf2d06233
Полина <kuranovapolina@gmail.com>
2021-10-13 23:56:15 +0300

    5. frequency hist 2 - added

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def print_dic(dic_name):
+    dic_name_list = sorted(dic_name.keys())
+    for letter in dic_name_list:
+        print("{0} : {1}".format(letter, dic_name[letter]))
+
+text = input("text: ")
+letters = {}
+
+for letter in list(text):
+    if letter in letters.keys():
+        letters[letter] += 1
+    else:
+        letters[letter] = 1
+
+letters_inv = {}
+
+for letter in letters.keys():
+    count = letters[letter]
+    if count in letters_inv:
+        letters_inv[count].append(letter)
+    else:
+        letters_inv[count] = [letter]
+
+print("Original:")
+print_dic(letters)
+
+print("\nInverted:")
+print_dic(letters_inv)
