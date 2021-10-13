9a3ffecc464a47fe599301c8ccb397a9b059644d
Полина <kuranovapolina@gmail.com>
2021-10-13 22:23:06 +0300

    2. geography - added

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+N = int(input("N = "))
+countries = {}
+
+for i in range(N):
+    st = input("{0} country:".format(i+1))
+    st_list = st.split(" ")
+    countries[st_list[0]] = st_list[1:]
+
+for i in range(3):
+    city = input("{0} city:".format(i+1))
+    for country in countries.keys():
+        if countries[country].count(city) == 1:
+            print("City {0} is located in country {1}".format(city, country))
+        elif countries[country].count(city) == 0:
+            print("No information")
