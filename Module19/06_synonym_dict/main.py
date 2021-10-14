770cb166a081a254c64f0128f4ff990716e91a14
Полина <kuranovapolina@gmail.com>
2021-10-14 21:29:24 +0300

    6. synonym dict - added

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+N = int(input("N = "))
+
+synonym_dict = {}
+
+for i in range(N):
+    pair = input("{0} pair: ".format(i+1)).lower().split(" - ")
+    synonym_dict[pair[0]] = pair[1]
+    synonym_dict[pair[1]] = pair[0]
+
+while True:
+    word = input("Input word: ").lower()
+
+    if word in synonym_dict.keys():
+        print("Synonym:", synonym_dict[word])
+        break
+    else:
+        print("Word not find")
