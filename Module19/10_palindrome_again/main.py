f463c8ce2e6229f6e57872d721263052754953f6
Полина <kuranovapolina@gmail.com>
2021-10-14 23:15:21 +0300

    10. palindrome - added

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+string = input('Input string: ')
+
+letters = {}
+
+for letter in string:
+    if letter in letters.keys():
+        letters[letter] = int(letters.pop(letter)) + 1
+    else:
+        letters[letter] = 1
+
+count = 0
+for letter in letters.keys():
+    if (int(letters[letter]) % 2) != 0:
+        count += 1
+
+if count > 1:
+    print("You can't make a palindrom")
+else:
+    print("You can make a palindrom")
