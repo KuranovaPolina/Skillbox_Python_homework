c78146df25913b87f76056180ac2bb93226e08e3
Полина <kuranovapolina@gmail.com>
2021-10-14 22:37:44 +0300

    8. guess number - added

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+N = input("N = ")
+
+data = {
+    "Да":[],
+    "Нет":[]
+}
+
+while True:
+    question = input("Нужное число есть среди вот этих чисел: ")
+
+    if question != "Помогите!":
+        answer = input("Ответ Артёма: ")
+        data[answer].extend(question.split(" "))
+    else:
+        break
+
+result = sorted(list(set(data["Да"]) - set(data["Нет"])))
+for i in result:
+    print(i, end = " ")
