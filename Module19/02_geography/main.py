751dd4874967d022e92a34c4a1636ef205cefaf6
Полина <kuranovapolina@gmail.com>
2021-10-15 19:28:40 +0300

    2. geography - modified

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -7,25 +7,13 @@ for i in range(N):
     countries[st_list[0]] = st_list[1:]
 
 for i in range(3):
+    find = False
     city = input("{0} city:".format(i + 1))
     for country in countries.keys():
         if countries[country].count(city) == 1:
             print("City {0} is located in country {1}".format(city, country))
-        elif countries[country].count(city) == 0:
-            print("No information")
+            find = True
+            break
+    if not find:
+        print("No information")
 
-# TODO: Лишние и некорректные print'ы:
-"""
-N = 2
-1 country:Россия Москва Питер
-2 country:Испания Барса Мадрид
-1 city:Барса
-No information
-City Барса is located in country Испания
-2 city:Москва
-City Москва is located in country Россия
-No information
-3 city:Лондон
-No information
-No information
-"""
