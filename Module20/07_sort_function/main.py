178f31ea685c6b1b09f75018b384c0a13695cd9a
kuran <kuranovapolina@gmail.com>
2021-10-19 23:32:19 +0300

    7. sort function - added

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def tpl_sort(tpl):
+    for element in tpl:
+        if not isinstance(element, int):
+            return tpl
+    return tuple(sorted(tpl))
+
+
+test_tpl = (17, 34, 6, 2, 3, 4, 5, 6, 19, 9, 10)
+print(tpl_sort(test_tpl))
