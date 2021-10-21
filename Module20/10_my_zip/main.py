2b14f125522085aa1b90d8ba4008ebf3258f6cd5
kuran <kuranovapolina@gmail.com>
2021-10-21 22:32:26 +0300

    10. my zip - added

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def my_zip(string, tpl):
+    string_list = list(string)
+    result = []
+
+    for i in range(min(len(string_list), len(tpl))):
+        new_pair = (string_list[i], tpl[i])
+        result.append(new_pair)
+
+    return result
+
+
+input_string = input("String: ")
+number_tuple = tuple((input("Tuple: "))[1:][:-1].split(", "))
+
+print(zip(tuple(input_string), number_tuple))
+for i in my_zip(input_string, number_tuple):
+    print(i)
+
