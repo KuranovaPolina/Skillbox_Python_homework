ac8d2afb94390c4803076cc217f0a31399408be3
kuran <kuranovapolina@gmail.com>
2021-10-23 15:54:26 +0300

    9. competition protocol - added

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+protocol = {}
+
+N = int(input("N = "))
+
+for i in range(N):
+    game = input("{0}: ".format(i + 1))
+    result, name = game.split(" ")
+
+    if name in protocol.keys() and protocol[name] < result:
+        protocol.pop(name)
+        protocol[name] = result
+    else:
+        protocol[name] = result
+
+sorted_protocol_keys = sorted(protocol, key=protocol.get)
+
+print("")
+for i in range(3):
+    name = sorted_protocol_keys[i]
+    print("{0} level. {1} ({2})".format(i + 1, name, protocol[name]))
