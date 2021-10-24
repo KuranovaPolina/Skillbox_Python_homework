628ebd099c8999929eda3e68162ec13d44a5cdfc
kuran <kuranovapolina@gmail.com>
2021-10-24 17:38:10 +0300

    9. competition protocol - fixed

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -5,22 +5,21 @@ N = int(input("N = "))
 for i in range(N):
     game = input("{0}: ".format(i + 1))
     result, name = game.split(" ")
+    result = int(result)
 
     if name in protocol.keys() and protocol[name] < result:
         protocol.pop(name)
-        # TODO: Количество баллов нужно переводить в тип int
         protocol[name] = result
     else:
         protocol[name] = result
 
-sorted_protocol_keys = sorted(protocol, key=protocol.get)
+sorted_protocol_keys = sorted(protocol, key=protocol.get, reverse=True)
 
 print("")
 for i in range(3):
     name = sorted_protocol_keys[i]
     print("{0} level. {1} ({2})".format(i + 1, name, protocol[name]))
 
-# TODO: Соритровка работает неверно:
 """
 N = 4
 1: 69485 Jack
