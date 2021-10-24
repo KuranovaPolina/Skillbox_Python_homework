e37f4ccbd4462938c745fb3a10d8516a49bab252
kuran <kuranovapolina@gmail.com>
2021-10-24 16:59:48 +0300

    2. universal program 2 - fixed

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -13,11 +13,9 @@ def is_prime(number):
 
 
 def f(object_list):
-    return [list(object_list)[i] for i in range(len(list(object_list))) if is_prime(i)]
+    return [list(object_list)[i] for i in range(2, len(list(object_list))) if is_prime(i)]
 
 
 test_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 print(f(test_tuple))
-
-# TODO: 1 не является простым числом
