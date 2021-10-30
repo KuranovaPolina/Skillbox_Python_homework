2fb6c5b0d90f1b35407284269d523af3f4d33ce4
kuran <kuranovapolina@gmail.com>
2021-10-30 22:05:18 +0300

    8. list of lists 2 - added

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,16 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+
+def make_list(lst):
+    all_element_list = []
+    for element in lst:
+        if isinstance(element, list):
+            all_element_list.extend(make_list(element))
+        else:
+            all_element_list.append(element)
+
+    return all_element_list
+
+
+print(make_list(nice_list))
