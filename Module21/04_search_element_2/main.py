2746f3de9f7d7d58d11641a33cfd7c7e36f8c540
kuran <kuranovapolina@gmail.com>
2021-10-30 20:26:31 +0300

    4. search element 2 - added

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,1 +1,41 @@
-# TODO здесь писать код
+site = {
+    'html': {
+        'head': {
+            'title': 'Мой сайт'
+        },
+        'body': {
+            'h2': 'Здесь будет мой заголовок',
+            'div': 'Тут, наверное, какой-то блок',
+            'p': 'А вот здесь новый абзац'
+        }
+    }
+}
+
+
+def find_key(dictionary, key, depth, now = 0):
+    if depth != -1 and depth < now:
+        return
+
+    if key in dictionary:
+        return dictionary[key]
+
+    for new_dictionary in dictionary.values():
+        if isinstance(new_dictionary, dict):
+            result = find_key(new_dictionary, key, depth, now + 1)
+
+            if result:
+                break
+
+    else:
+        result = None
+
+    return result
+
+
+user_key = input("Key: ")
+depth = int(input("Input depth (if not need, write 0)"))
+result = find_key(site, user_key, depth - 1)
+if result:
+    print(result)
+else:
+    print("Not found")
