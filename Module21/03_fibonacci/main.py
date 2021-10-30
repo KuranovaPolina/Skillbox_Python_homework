cb9e9c2f6d8850ae76f3e7d3e04e54beeac00576
kuran <kuranovapolina@gmail.com>
2021-10-30 22:02:07 +0300

    3. fibonacci - added

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def find_fibonacci_num(pos):
+    if pos in (1, 2):
+        return 1
+    return find_fibonacci_num(pos - 1) + find_fibonacci_num(pos - 2)
+
+
+num_pos = int(input("Location: "))
+print("Number:", find_fibonacci_num(num_pos))
