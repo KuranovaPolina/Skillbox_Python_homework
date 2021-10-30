6ea89ecc12392a07fa7cc8de207c7fc00f666c6d
kuran <kuranovapolina@gmail.com>
2021-10-30 21:06:13 +0300

    5. make function faster - added

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,29 @@
-def calculating_math_func(data):
+def calculating_math_func_start(data):
     result = 1
     for index in range(1, data + 1):
         result *= index
     result /= data ** 3
-    result = result ** 10
+    result = (result ** 10)
     return result
 
-# TODO оптимизировать функцию
+
+def calculating_math_func(data, factorials={1: 1}):
+    if data in factorials:
+        result = factorials[data]
+    else:
+        result = 1
+        for index in range(data, 0, -1):
+            if index in factorials:
+                result *= factorials[index]
+                break
+            else:
+                result *= index
+        factorials[data] = result
+    result /= data ** 3
+    result = result ** 10
+    return result, factorials
+
+
+print(calculating_math_func_start(6))
+print(calculating_math_func(5)[0])
+print(calculating_math_func(6)[0])
