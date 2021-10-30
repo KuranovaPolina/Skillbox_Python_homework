6101357088aae29e639fa382093d3f587b4d958f
kuran <kuranovapolina@gmail.com>
2021-10-30 21:56:33 +0300

    7. advanced sum - added

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def sum_(*args):
+    def make_list(lst):
+        all_element_list = []
+        for element in lst:
+            if isinstance(element, list):
+                all_element_list.extend(make_list(element))
+            else:
+                all_element_list.append(element)
+
+        return all_element_list
+
+    result = 0
+    for value in make_list(list(args)):
+        result += value
+
+    return result
+
+
+print(sum_([[1, 2, [3]], [1], 3]))
