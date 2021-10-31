1a3a0d88b3f575e648453a550024267737ef7d69
kuran <kuranovapolina@gmail.com>
2021-10-31 13:56:39 +0300

    6. deep copy - added

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,3 +1,5 @@
+import copy
+
 site = {
     'html': {
         'head': {
@@ -11,4 +13,33 @@ site = {
     }
 }
 
-# TODO здесь писать код
+
+def sites_func(phone_name_list):
+    for i in phone_name_list:
+        new_site = copy.deepcopy(site)
+        new_site['html']['head']['title'] = 'Куплю/продам {0} недорого'.format(i)
+        new_site['html']['body']['h2'] = 'У нас самая низкая цена на {0}'.format(i)
+        print(i)
+        print("site =", new_site)
+
+
+N = int(input("N = "))
+products_list = []
+for _ in range(N):
+    product_name = input("Product name: ")
+    products_list.insert(0, product_name)
+    sites_func(products_list)
+
+
+'''
+# version with recursion - problem
+if i == "телефон":
+    new_site = copy.deepcopy(site)
+    return new_site
+else:
+    new_site = sites_func(phone_name_list[1:])
+    new_site['html']['head']['title'] = 'Куплю/продам {0} недорого'.format(i)
+    new_site['html']['body']['h2'] = 'У нас самая низкая цена на {0}'.format(i)
+    print(i)
+    print(new_site)
+'''
