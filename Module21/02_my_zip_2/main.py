fce0beb09bd9d26fc528331d0bdbbc9fecdd8120
kuran <kuranovapolina@gmail.com>
2021-10-31 12:08:08 +0300

    2. my zip 2 - added

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+def my_zip(*arg):
+    arg = list(map(list, arg))
+    try:
+        yield tuple(map(lambda x: x.pop(0), arg))
+        yield from my_zip(*arg)
+    except:
+        return
+
+
+input_string = 'abcd'
+input_tuple = (1, 2, 10)
+input_dict = {'a': 1, 'b': 2, 'c': 3}
+input_list = [4, 6, 8, 10, 11]
+print(my_zip(input_string, input_tuple, input_dict, input_list))
+for i in my_zip(input_string, input_tuple, input_dict, input_list):
+    print(i)
