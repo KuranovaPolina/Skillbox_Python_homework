186f93fbd009475230993bd72ad82a5e774d5dba
kuran <kuranovapolina@gmail.com>
2021-11-04 23:40:48 +0300

    2. zen of python - added

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import os
+
+path = os.path.abspath('zen.txt')
+
+zen_file = open(path, 'r')
+
+lines = []
+for i_line in zen_file:
+    lines.append(i_line)
+
+zen_file.close()
+
+for i_line in lines[::-1]:
+    print(i_line, end='')
