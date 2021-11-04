d4cc0eeb4d144eff909298bc10f70ca4dae39b7d
kuran <kuranovapolina@gmail.com>
2021-11-04 23:21:38 +0300

    1. 1 nums sum 2 - added

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+import os
+
+path = os.path.abspath('numbers.txt')
+file = open(path, 'r')
+file_text = file.read()
+file.close()
+result = 0
+for i_element in file_text:
+    if i_element.isdigit():
+        result += int(i_element)
+
+path = os.path.abspath('answer.txt')
+file = open(path, 'w')
+file.write(f"result {result}")
+file.close()
