3ad0404516ba9d72b8f2fd0481f7b1a40389506c
kuran <kuranovapolina@gmail.com>
2021-11-05 00:07:24 +0300

    4. files and folders - added

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import os
+
+path = input("Input path: ")
+# path = os.path.join('..', '..', '..', 'python_basic')
+files = os.listdir(path)
+size = 0
+folder_count = 0
+file_count = 0
+for file in files:
+    file_path = os.path.join(path, file)
+    size += os.path.getsize(file_path) / 1024
+    if os.path.isfile(file_path):
+        file_count += 1
+    else:
+        folder_count += 1
+
+print(size)
+print(folder_count)
+print(file_count)
