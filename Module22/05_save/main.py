be50ac2aeca58e2e899076eb0efb091469f1742b
kuran <kuranovapolina@gmail.com>
2021-11-06 22:17:58 +0300

    5. save - added

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import os
+
+string = input("Input string: ")
+path_list = input("Input path (use " "): ").split(" ")
+file_name = input("Input file name: ")
+path = "C:" + os.path.sep + os.path.sep.join(path_list) + os.path.sep + file_name + ".txt"
+print(path)
+if os.path.exists(path):
+    answer = input("Rewrite file? (True / False)")
+    if answer == "True":
+        file = open(path, 'w')
+        file.write(string)
+        file.close()
+        print("Rewrited")
+else:
+    file = open(path, 'w')
+    file.write(string)
+    file.close()
+    print("writed")
