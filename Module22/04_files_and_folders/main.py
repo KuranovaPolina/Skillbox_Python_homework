c55bba3e9746322bc60d21675b965593c7083bef
kuran <kuranovapolina@gmail.com>
2021-11-07 18:45:01 +0300

    4. files and folders - edited

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,21 +1,38 @@
 import os
 
+
+def folder_size_f(folder_path):
+    folder_files = os.listdir(folder_path)
+    folder_size = 0
+    for folder_file in folder_files:
+        folder_file_path = os.path.join(folder_path, folder_file)
+        if os.path.isfile(folder_file_path):
+            folder_size += os.path.getsize(folder_file_path)
+        else:
+            folder_size += folder_size_f(folder_file_path)
+
+    return folder_size
+
+
 # path = input("Input path: ")
-path = os.path.join('..', '..', 'Module14')
+path = os.path.join('..', '..', 'Module22')
+print(path)
 files = os.listdir(path)
 size = 0
 folder_count = 0
 file_count = 0
+
 for file in files:
     file_path = os.path.join(path, file)
-    size += os.path.getsize(file_path) / 1024
     if os.path.isfile(file_path):
         file_count += 1
+        size += os.path.getsize(file_path)
     else:
         folder_count += 1
+        size += folder_size_f(file_path)
 
-print(size)
+
+print(size / 1024)
 print(folder_count)
 print(file_count)
 
-# TODO: Не считает размер и количество файлов
