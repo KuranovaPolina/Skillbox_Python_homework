2c53ab707bca93e14d5f0b2d46e3da71cff1f074
kuran <kuranovapolina@gmail.com>
2021-11-12 23:31:30 +0300

    1. names 2 - added

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def func(line):
+    line_len = len(line)
+    if line[-1] == '\n':
+        line_len -= 1
+    try:
+        if line_len < 3:
+            raise BaseException
+    except BaseException:
+        print(f"{len_number} line - len < 3")
+        errors_file.write(f"{len_number} line - len < 3" + "\n")
+    return line_len
+
+
+len_number = 0
+general_len = 0
+file_text = ""
+errors_file = open('errors.logs', 'w')
+try:
+    with open('people.txt', 'r') as file:
+        for i_line in file:
+            len_number += 1
+            general_len += func(i_line)
+
+except FileNotFoundError:
+    print("File not found")
+
+finally:
+    print(general_len)
