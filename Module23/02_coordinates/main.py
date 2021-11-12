e07659f3b0969fa3c187c26b11115c35f2c79141
kuran <kuranovapolina@gmail.com>
2021-11-12 22:06:29 +0300

    2. coordinates - added

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -14,26 +14,17 @@ def f2(x, y):
 
 
 try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
-        nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
+    with open('coordinates.txt', 'r') as file:
+        file_2 = open('result.txt', 'w')
+        for line in file:
+            nums_list = line.split(" ")
+            res1 = f(int(nums_list[0]), int(nums_list[1]))
             res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
-except Exception:
-    print("Что-то пошло не так с первой функцией")
+            number = random.randint(0, 100)
+            my_list = sorted([str(res1), str(res2), str(number)])
+            file_2.write(str(' '.join(my_list)) + '\n')
 
-
-# TODO отредактировать и исправить программу
+except FileNotFoundError:
+    print("File not found")
+except ZeroDivisionError:
+    print("Zero division")
