62728f2890a314f41612d1860d50876c13bfb90e
kuran <kuranovapolina@gmail.com>
2021-11-12 22:37:48 +0300

    3. lucky number - added

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import random
+
+
+count = 0
+with open('result.txt', 'w') as file:
+    while count < 777:
+        random_exception = random.randint(1, 39)
+        if random_exception == 1:
+            raise Exception("End")
+        elif random_exception == 2:
+            raise ArithmeticError("End")
+        elif random_exception == 3:
+            raise TypeError("End")
+        else:
+            number = int(input("Number:"))
+            count += number
+            file.write(str(number) + '\n')
