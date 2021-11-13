57dee4bbd3f9bcf4f32c91cffd32e75b82684ca2
kuran <kuranovapolina@gmail.com>
2021-11-13 23:42:20 +0300

    5. text_calc - added

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,50 @@
-# TODO здесь писать код
+def count_func(line):
+    line_list = line.split(" ")
+    line_len = len(line_list)
+    if line_list[-1] == '\n':
+        line_len -= 1
+    if len(line_list) != 3 or not (line_list[1] in operands) or \
+            not (line_list[0].isdigit() and line_list[2].isdigit()):
+        raise IndexError
+    elif line_list[1] == "+":
+        return int(line_list[0]) + int(line_list[2])
+    elif line_list[1] == "-":
+        return int(line_list[0]) - int(line_list[2])
+    elif line_list[1] == "*":
+        return int(line_list[0]) * int(line_list[2])
+    elif line_list[1] == "/":
+        if line_list[2] == "0":
+            raise ZeroDivisionError
+        else:
+            return int(line_list[0]) / int(line_list[2])
+    elif line_list[1] == "**":
+        return int(line_list[0]) ** int(line_list[2])
+    elif line_list[1] == "%":
+        if line_list[2] == "0":
+            raise ZeroDivisionError
+        else:
+            return int(line_list[0]) % int(line_list[2])
+    elif line_list[1] == "//":
+        if line_list[2] == "0":
+            raise ZeroDivisionError
+        else:
+            return int(line_list[0]) // int(line_list[2])
+
+
+operands = ["+", "-", "*", "/", "**", "%", "//"]
+result = 0
+try:
+    with open('calc.txt', 'r') as file:
+        for i_line in file:
+            try:
+                if i_line[-1] == '\n':
+                    i_line = i_line[:-1]
+                result += count_func(i_line)
+            except IndexError:
+                print("Wrong format")
+            except ZeroDivisionError:
+                print("Zero division error")
+    print(result)
+except FileNotFoundError:
+    print("File not found")
+
