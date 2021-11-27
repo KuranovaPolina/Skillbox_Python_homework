08135c71f3acbad21381d41bc8225f70bda6131e
kuran <kuranovapolina@gmail.com>
2021-11-27 22:31:05 +0300

    3. my dict - added

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def __init__(self, dict):
+        self.dict = dict
+
+    def get(self, key):
+        res = dict(self).get(key)
+        if res == None:
+            return 0
+        else:
+            return res
+
+
+dict1 = {1: 1}
+dict2 = MyDict(dict1)
+
+print(dict1.get(2))
+print(dict2.get(2))
