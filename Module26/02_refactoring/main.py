a500009840c13340512824af6714f3a02543449c
kuran <kuranovapolina@gmail.com>
2021-12-07 10:56:38 +0300

    2. refactoring - added

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -2,16 +2,15 @@ list_1 = [2, 5, 7, 10]
 list_2 = [3, 8, 4, 9]
 to_find = 56
 
-can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
-        print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
-        break
 
-# TODO провести рефакторинг кода
+def finder(list1: list, list2: list) -> int:
+    for x in list1:
+        for y in list2:
+            print(x, y, x * y)
+            yield x * y
+
+
+for result in finder(list_1, list_2):
+    if result == to_find:
+        print('Found!!!')
+        break
