eae967a6f731436129b2f485a1fce966d0e947c8
kuran <kuranovapolina@gmail.com>
2021-12-09 12:24:07 +0300

    4. hot seq - added

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def QHofstadter(s: list) -> int:
+    elements = s
+    while True:
+        try:
+            new_el = elements[-elements[-1]] + elements[-elements[-2]]
+            elements.append(new_el)
+            yield new_el
+        except IndexError:
+            break
+
+
+# не понимаю, почему она должна останавливаться на [1, 2], а не на [1, 3]
+first_line = QHofstadter([1, 2])
+for _ in range(20):
+    try:
+        print(next(first_line))
+    except StopIteration:
+        break
