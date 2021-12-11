e58d30956fcc39bf34a3783b44422932aa39850f
kuran <kuranovapolina@gmail.com>
2021-12-11 20:31:31 +0300

    1. num squares - edited

--- Module26/01_num_squares/main.py
+++ Module26/01_num_squares/main.py
@@ -3,16 +3,14 @@ class Iter:
         self.__N = N
         self.__counter = 0
 
-    # TODO: Здесь возвращается либо число, либо ничего
-    def __next__(self):
+    def __next__(self) -> int or None:
         if self.__counter < self.__N:
             self.__counter += 1
             return self.__counter ** 2
         else:
             raise StopIteration
 
-    # TODO: Для аннотации в данном случае используется название класса в кавычках
-    def __iter__(self):
+    def __iter__(self) -> "Iter":
         return self
 
 
@@ -33,5 +31,3 @@ N = int(input("N = "))
 it3 = (x ** 2 for x in range(1, N + 1))
 for i in it3:
     print(i)
-
-# что делать с аннотацией next и init
