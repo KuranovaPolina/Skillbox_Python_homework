87753af8fbd6a734577a06aa7a751c7c1d63b8bd
kuran <kuranovapolina@gmail.com>
2021-12-25 10:44:44 +0300

    1. how are you - added

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def how_are_you(function_to_decorate):
+    def wrapped_func(*args):
+        print("Как дела? Хорошо\nА у меня не очень! Ладно, держи свою функцию.")
+        function_to_decorate(*args)
+    return wrapped_func
+
+
+@how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+@how_are_you
+def helloWorld(mess):
+    print(mess)
+
+
+test()
+helloWorld("Hello world")
