724581f319634f0639cb4db1c99845d13008378a
kuran <kuranovapolina@gmail.com>
2021-12-25 11:47:03 +0300

    2. slowdown - edited

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -2,14 +2,14 @@ import time
 
 
 def slower(function_to_decorate):
-    def wrapped_func(*args):
+    def wrapped_func(*args) -> None:
         time.sleep(3)
         function_to_decorate(*args)
     return wrapped_func
 
 
 @slower
-def test():
+def test() -> None:
     print('<Тут что-то происходит...>')
 
 
