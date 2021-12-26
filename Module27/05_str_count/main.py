eed94fd8baa637e5361f03407b4869514b08a0d2
kuran <kuranovapolina@gmail.com>
2021-12-26 15:54:09 +0300

    5. str count - added

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def counter(func):
+    def wrapper(*args, **kwargs) -> None:
+        wrapper.count += 1
+        func(*args, **kwargs)
+
+    wrapper.count = 0
+    return wrapper
+
+
+@counter
+def f() -> None:
+    print("Doing something")
+
+
+f()
+f()
+f()
+
+print(f.count)
