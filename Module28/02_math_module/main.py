1efd2fee7775b7e86c49606cfd99c94bf8f32ac9
kuran <kuranovapolina@gmail.com>
2021-12-30 15:36:05 +0300

    2. math module - edited

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,6 +1,6 @@
 import my_math
 
-res_1 = my_math.MyMath.circle_len(my_math.MyMath(), size=5)
-res_2 = my_math.MyMath.circle_square(my_math.MyMath(), size=6)
+res_1 = my_math.MyMath.circle_len(5)
+res_2 = my_math.MyMath.circle_square(6)
 print(res_1)
 print(res_2)
--- Module28/02_math_module/my_math.py
+++ Module28/02_math_module/my_math.py
@@ -2,14 +2,18 @@ import math
 
 
 class MyMath:
-    def circle_len(self, size=0) -> float:
+    @classmethod
+    def circle_len(cls, size=0) -> float:
         return 2 * math.pi * size
 
-    def circle_square(self, size=0) -> float:
+    @classmethod
+    def circle_square(cls, size=0) -> float:
         return math.pi * size ** 2
 
-    def cube_volume(self, size=0) -> float:
+    @classmethod
+    def cube_volume(cls, size=0) -> float:
         return size ** 3
 
-    def sphere_size(self, size=0) -> float:
+    @classmethod
+    def sphere_size(cls, size=0) -> float:
         return 4 * math.pi * size ** 2
