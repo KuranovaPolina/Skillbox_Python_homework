5c99e67d0d36ff35222dd3fe0ab7361317e597c8
kuran <kuranovapolina@gmail.com>
2021-12-30 13:07:33 +0300

    1. work with file 2 - added

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+from typing import TextIO
+
+
+class File:
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.file = None
+        self.found = False
+
+    def __enter__(self) -> tuple[TextIO, bool]:
+        try:
+            self.file = open(self.filename, encoding='utf-8')
+            self.found = True
+        except FileNotFoundError:
+            self.file = open(self.filename, 'w', encoding='utf-8')
+        return self.file, self.found
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.file.close()
+
+
+with File('example.txt') as info:
+    if not info[1]:
+        info[0].write('Всем привет')
+    else:
+        print(info[0].read())
