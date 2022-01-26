31045798aff0aef5cdb0eb360ac3c6fd8e66ceca
kuran <kuranovapolina@gmail.com>
2022-01-26 15:19:06 +0300

    2. callback - added

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+import functools
+from typing import Callable, Any
+
+
+app = {}
+
+
+def callback(key: str) -> Callable:
+    def call_func(func: Callable) -> Callable:
+        app[key] = func
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            return func(*args, **kwargs)
+        return wrapper
+    return call_func
+
+
+@callback('//')
+def example() -> str:
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
