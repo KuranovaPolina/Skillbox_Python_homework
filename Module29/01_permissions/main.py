13a88916a204190a408e622bb26081ff67e40fbc
kuran <kuranovapolina@gmail.com>
2022-01-26 13:05:14 +0300

    1. permission - added

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+import functools
+from collections.abc import Callable
+
+
+def check_permission(user: str) -> Callable:
+    def check(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            if user in user_permissions:
+                return func(*args, **kwargs)
+            else:
+                print(f"PermissionError:"
+                      f" У пользователя недостаточно прав, "
+                      f"чтобы выполнить функцию {func.__name__}")
+        return wrapped
+    return check
+
+
+user_permissions = ['admin']
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
