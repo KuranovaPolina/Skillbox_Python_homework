1cc094bdb75ae5a8602cfe75e06f2dc711d09472
kuran <kuranovapolina@gmail.com>
2022-01-27 17:29:20 +0300

    5. singleton - added

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+
+def singleton(cls) -> Callable:
+    instances = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return wrapper
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
