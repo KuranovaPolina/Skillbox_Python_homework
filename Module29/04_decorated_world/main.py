a21e2c530518baa19032a67d1499bbf26b785847
kuran <kuranovapolina@gmail.com>
2022-01-27 17:10:01 +0300

    4. decorated world - added

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+
+def decorator_with_args_for_any_decorator(decorator: Callable) -> Callable:
+    def decorator_args(*args, **kwargs) -> Callable:
+        def wrapper(func) -> Any:
+            return decorator(func, *args, **kwargs)
+        return wrapper
+    return decorator_args
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable, *args, **kwargs) -> Callable:
+    def wrapper(*fargs, **fkwargs) -> Any:
+        print("Переданные арги и кварги в декоратор:", args, kwargs)
+        return func(*fargs, **fkwargs)
+    return wrapper
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function("Юзер", 101)
