8953c2ebec9b2d88d3d666f51f07cfa21fccd625
kuran <kuranovapolina@gmail.com>
2022-01-29 17:02:15 +0300

    5. palindrome the return - added

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+import collections
+
+
+def can_be_poly(string: str) -> bool:
+    letters = collections.Counter(string)
+
+    count = len(list(filter(lambda x: int(letters[x]) % 2 != 0, letters)))
+
+    if count > 1:
+        return False
+    else:
+        return True
+
+
+print(can_be_poly('ababc'))
+print(can_be_poly('abbbc'))
