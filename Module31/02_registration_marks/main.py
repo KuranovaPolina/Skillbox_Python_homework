30b56cd6cb2ee1856765d3392a3358b0824ce067
kuran <kuranovapolina@gmail.com>
2022-02-01 14:51:09 +0300

    2. registration marks - added

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import re
+
+
+text = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666'
+
+own_car = re.findall(r'\b[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}', text)
+taxi = re.findall(r'\b[АВЕКМНОРСТУХ]{2}\d{5,6}', text)
+
+print(own_car)
+print(taxi)
