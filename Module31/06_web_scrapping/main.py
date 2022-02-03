3e6e865abb8d108f32caedfcca6fb1df1e6477ce
kuran <kuranovapolina@gmail.com>
2022-02-03 15:37:42 +0300

    6. web scrapping - added

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import requests
+import re
+
+
+my_rec = requests.get('http://www.columbia.edu/~fdc/sample.html')
+labels = re.findall(r'<h3.*/h3>', my_rec.text)
+
+for i in range(len(labels)):
+    labels[i] = re.findall(r'>.*<', labels[i])[0][1:-1]
+
+print(labels)
